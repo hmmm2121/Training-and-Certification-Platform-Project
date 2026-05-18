b43b93188b109af8256fcbdcd22444eb595c9329
hmmm2121 <hussainnader771@gmail.com>
2026-05-18 21:08:29 +0300

    created the api
    added swagger api and moved the data and models from the mvc to the api project

---
+++ TrainingAndCertificationPlatform/TrainingAndCertificationAPI/Models/Assessment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrainingAndCertificationPlatform.Models;
+
+public partial class Assessment
+{
+    [Key]
+    public int AssessmentId { get; set; }
+
+    public int EnrollmentId { get; set; }
+
+    [StringLength(100)]
+    public string Result { get; set; } = null!;
+
+    [StringLength(100)]
+    public string? Notes { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? RecordedAt { get; set; }
+
+    [ForeignKey("EnrollmentId")]
+    [InverseProperty("Assessments")]
+    public virtual Enrollment Enrollment { get; set; } = null!;
+}
---
+++ TrainingAndCertificationPlatform/TrainingAndCertificationAPI/Models/RoomEquipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrainingAndCertificationPlatform.Models;
+
+[Table("RoomEquipment")]
+public partial class RoomEquipment
+{
+    [Key]
+    public int EquipmentId { get; set; }
+
+    public int RoomId { get; set; }
+
+    [StringLength(100)]
+    public string EquipmentName { get; set; } = null!;
+
+    [ForeignKey("RoomId")]
+    [InverseProperty("RoomEquipments")]
+    public virtual Room Room { get; set; } = null!;
+}
